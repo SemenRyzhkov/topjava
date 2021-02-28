650c4b23e86615658d64ee64ea4056eb234f00b0
SemenRyzhkov <thaisun2018@yandex.ru>
2021-02-28 16:54:15 +0300

    HW3

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,7 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2021-01-22 13:21', 'dinner', 1550),
+       (100001, '2021-01-21 19:40', 'lunch', 1770);
