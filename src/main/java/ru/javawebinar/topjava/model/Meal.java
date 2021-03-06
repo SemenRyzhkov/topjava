f05580104ac8a168f903c1683a602bfa9cad98bd
SemenRyzhkov <thaisun2018@yandex.ru>
2021-03-06 21:03:51 +0300

    HW4

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,15 +1,55 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.UPDATE, query = "UPDATE Meal m " +
+                "SET m.dateTime=:date_time, m.description=:description, m.calories=:calories " +
+                "WHERE m.id=:id AND m.user.id=?1"),
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=?1 AND m.user.id=?2"),
+        @NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m  WHERE m.id=?1 AND m.user.id=?2"),
+        @NamedQuery(name = Meal.BETWEEN_HALF_OPEN, query = "SELECT m FROM Meal m WHERE m.user.id=?1 " +
+                "AND m.dateTime>=?2 AND m.dateTime<?3 ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id=?1 ORDER BY m.dateTime DESC")
+})
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"},
+        name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String UPDATE = "Meal.update";
+    public static final String DELETE = "Meal.delete";
+    public static final String GET = "Meal.get";
+    public static final String BETWEEN_HALF_OPEN = "Meal.getBetweenHalfOpen";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm")
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(min = 5, max = 100)
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @Range(min = 10, max = 10000)
     private int calories;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Meal() {
     }
@@ -55,6 +95,14 @@ public class Meal extends AbstractBaseEntity {
 
     public void setCalories(int calories) {
         this.calories = calories;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override
---
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -0,0 +1,79 @@
+package ru.javawebinar.topjava.repository.jpa;
+
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Repository
+@Transactional(readOnly = true)
+public class JpaMealRepository implements MealRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    @Transactional
+    public Meal save(Meal meal, int userId) {
+        if (meal.isNew()) {
+            User ref = em.getReference(User.class, userId);
+            meal.setUser(ref);
+            em.persist(meal);
+            return meal;
+        } else {
+//            Meal updatedMeal = get(meal.getId(), userId);
+//            if (updatedMeal != null) {
+//                updatedMeal.setDateTime(meal.getDateTime());
+//                updatedMeal.setDescription(meal.getDescription());
+//                updatedMeal.setCalories(meal.getCalories());
+//                return em.merge(updatedMeal);
+//            } else return null;
+            List<Meal> meals = em.createNamedQuery(Meal.UPDATE)
+                    .setParameter(1, userId)
+                    .getResultList();
+            return DataAccessUtils.singleResult(meals);
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean delete(int id, int userId) {
+        return em.createNamedQuery(Meal.DELETE)
+                .setParameter(1, id)
+                .setParameter(2, userId)
+                .executeUpdate() != 0;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        List<Meal> meals = em.createNamedQuery(Meal.GET)
+                .setParameter(1, id)
+                .setParameter(2, userId)
+                .getResultList();
+        return DataAccessUtils.singleResult(meals);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return em.createNamedQuery(Meal.ALL_SORTED, Meal.class)
+                .setParameter(1, userId)
+                .getResultList();
+    }
+
+    @Override
+    public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return em.createNamedQuery(Meal.BETWEEN_HALF_OPEN, Meal.class)
+                .setParameter(1, userId)
+                .setParameter(2, startDateTime)
+                .setParameter(3, endDateTime)
+                .getResultList();
+    }
+}
